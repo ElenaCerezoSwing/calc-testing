e28521d2e9d5f771be450effe6e6bce3f204d3d6
Ele_hopper <elenacerezo83@gmail.com>
2018-05-25 09:53:32 +0200

    RED compare two differnt types of values. Expected number but received string

--- js/spec.js
+++ js/spec.js
@@ -11,6 +11,13 @@ describe("calculator machine adds", function () {
         expect(app.sum(0, 0)).toBe(0);
         expect(app.sum(1, 0)).toBe(1);
         expect(app.sum(-1, 0)).toBe(-1);
+        expect(app.sum(1.0, 0)).toBe(1);
+        expect(app.sum(1.0, 1.2)).toBe(2.2);
+        expect(app.sum(1.03, 1.2)).toBe(2.23);
+        expect(app.sum(1.03, 1.21)).toBe(2.24);
+        expect(app.sum(1.03, 1.97)).toBe(3);
+        expect(app.sum("1.03", 1.97)).toBe(3);
+
     });
 
 
