d4d200470950ec5234e9382ecf4dd659c5468e72
Ele_hopper <elenacerezo83@gmail.com>
2018-05-25 10:20:56 +0200

    RED app.rest is not a function

--- js/spec.js
+++ js/spec.js
@@ -5,22 +5,30 @@ beforeEach(function () {
     app = Application();
 });
 
-describe("calculator machine adds", function () {
+describe("calculator machine functions works", function () {
 
     it("really adds", function () {
         expect(app.sum(0, 0)).toBe(0);
         expect(app.sum(1, 0)).toBe(1);
         expect(app.sum(-1, 0)).toBe(-1);
+        expect(app.sum(-1, -1)).toBe(-2);
         expect(app.sum(1.0, 0)).toBe(1);
         expect(app.sum(1.0, 1.2)).toBe(2.2);
         expect(app.sum(1.03, 1.2)).toBe(2.23);
         expect(app.sum(1.03, 1.21)).toBe(2.24);
         expect(app.sum(1.03, 1.97)).toBe(3);
         expect(app.sum("1.03", 1.97)).toBe(3);
-
-
-
-
+    });
+    it("really rests", function () {
+        expect(app.rest(0, 0)).toBe(0);
+        expect(app.rest(1, 0)).toBe(1);
+        expect(app.rest(-1, 0)).toBe(-1);
+        expect(app.rest(-1, -1)).toBe(0);
+        expect(app.rest(1.0, 0)).toBe(1);
+        expect(app.rest(1.0, 1.2)).toBe(-0.2);
+        expect(app.rest(1.03, 1.01)).toBe(0.02);
+        expect(app.rest("1.03", 1)).toBe(0.03);
+        expect(app.rest("-1.03", 1)).toBe(-2.03);
     });
 
 
